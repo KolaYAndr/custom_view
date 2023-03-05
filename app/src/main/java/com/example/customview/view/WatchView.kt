32464222dc7180f7d29205816a0757f3814f0fe2
KolaYAndr <nikolay_andreev02@mail.ru>
2023-03-05 03:05:56 +0300

    Can draw current time once it is shown

--- app/src/main/java/com/example/customview/view/WatchView.kt
+++ app/src/main/java/com/example/customview/view/WatchView.kt
@@ -1,7 +1,130 @@
 package com.example.customview.view
 
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
 import android.view.View
+import java.time.LocalTime
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
-class WatchView : View() {
+class WatchView(context: Context, attributeSet: AttributeSet?, defStyleAttr: Int) :
+    View(context, attributeSet, defStyleAttr) {
+
+    private var timeInSeconds = LocalTime.now().toSecondOfDay()
+
+    private val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        canvas?.save()
+
+        //drawing elements
+        drawBackground(canvas)
+        drawScaleLines(canvas)
+        drawHands(canvas)
+
+        canvas?.restore()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+
+    }
+
+    private fun drawBackground(canvas: Canvas?){
+        canvas?.scale(.5f * width, -.5f * width)
+        canvas?.translate(1f, -1f)
+
+        paint.color = Color.BLACK
+        paint.style = Paint.Style.FILL
+
+        canvas?.drawCircle(0f, 0f, 1f, paint)
+
+        paint.color = Color.DKGRAY
+
+        canvas?.drawCircle(0f, 0f, 0.85f, paint)
+
+        paint.color = Color.GRAY
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = Constants.secondHandStrokeWidth
+    }
+
+    private fun drawScaleLines(canvas: Canvas?){
+        val step = 2 * PI / 60
+        val scale = Constants.normalScaleSize
+
+        for (i in 0..60) {
+            val x1 = cos(2 * PI - step * i).toFloat()
+            val y1 = sin(2 * PI - step * i).toFloat()
+
+            var x2: Float
+            var y2: Float
+
+            if (i % 5 == 0) {
+                x2 = x1 * Constants.largeScaleSize
+                y2 = y1 * Constants.largeScaleSize
+            } else {
+                x2 = x1 * scale
+                y2 = y1 * scale
+            }
+
+            canvas?.drawLine(x1, y1, x2, y2, paint)
+        }
+    }
+
+    private fun drawHands(canvas: Canvas?){
+        paint.color = Color.WHITE
+
+        drawHourHand(canvas)
+        drawMinuteHand(canvas)
+        drawSecondHand(canvas)
+    }
+
+    private fun drawHourHand(canvas: Canvas?){ //method draws hour hand
+        canvas?.save()
+
+        canvas?.rotate(-(timeInSeconds / 3600.0 * 30).toFloat())
+
+        paint.strokeWidth = Constants.hourHandStrokeWidth
+        canvas?.drawLine(0f, 0f, 0f, .6f, paint)
+
+        canvas?.restore()
+    }
+
+    private fun drawMinuteHand(canvas: Canvas?){
+        canvas?.save()
+
+        canvas?.rotate(-(timeInSeconds / 60.0 * 6).toFloat())
+
+        paint.strokeWidth = Constants.minuteHandStrokeWidth
+        canvas?.drawLine(0f, 0f, 0f, .75f, paint)
+
+        canvas?.restore()
+    }
+
+    private fun drawSecondHand(canvas: Canvas?){
+        canvas?.save()
+
+        canvas?.rotate(-timeInSeconds * 6f)
+
+        paint.strokeWidth = Constants.secondHandStrokeWidth
+        canvas?.drawLine(0f, 0f, 0f, .9f, paint)
+
+        canvas?.restore()
+    }
+
+    object Constants{
+        const val secondHandStrokeWidth = .01f
+        const val hourHandStrokeWidth = .05f
+        const val minuteHandStrokeWidth = .03f
+        const val normalScaleSize = .95f
+        const val largeScaleSize = .95f * .95f
+    }
 
 }
